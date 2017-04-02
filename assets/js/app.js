af45bae452172642ebe120148a48e787037e0952
philipp <fkolesnik@gmail.com>
2017-04-03 00:54:55 +0300

    blockquote

--- assets/js/app.js
+++ assets/js/app.js
@@ -76,4 +76,17 @@
 })();
 
 
+// Tabs
 
+// wire up shown event
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    e.preventDefault();
+    console.log("tab shown...");
+});
+
+// read hash from page load and change tab
+var hash = document.location.hash;
+var prefix = "tab_";
+if (hash) {
+    $('.nav-tabs a[href="'+hash.replace(prefix,"")+'"]').tab('show');
+}
