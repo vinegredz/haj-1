4b4b99e2dc89dadb0e4eb658e68e35314d1f9ed3
philipp <fkolesnik@gmail.com>
2017-04-06 21:47:22 +0300

    permanent links to guides sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+
+
 // On load
 (function () {
 
@@ -108,18 +110,22 @@
 
 // Keep selected tab on page refresh
 (function () {
-    $('#myTab a').click(function (e) {
+
+    var tabsLink = $('#tabs-guide a');
+
+    tabsLink.click(function (e) {
         e.preventDefault();
         $(this).tab('show');
     });
 
-// store the currently selected tab in the hash value
-    $("ul.nav-tabs > li > a").on("shown.bs.tab", function (e) {
-        var id = $(e.target).attr("href").substr(1);
-        window.location.hash = id;
+    tabsLink.on("shown.bs.tab", function (e) {
+        var idLink = $(e.target).attr("href").substr(1);
+        window.location.hash = idLink;
+        $(window).scrollTop(0);
     });
 
 // on load of the page: switch to the currently selected tab
-    var hash = window.location.hash;
-    $('#myTab a[href="' + hash + '"]').tab('show');
+    var hashLink = window.location.hash;
+    $('#tabs-guide a[href="' + hashLink + '"]').tab('show');
+
 })();
