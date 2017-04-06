391237b8d88d23d3505f966eeabf87af18606589
philipp <fkolesnik@gmail.com>
2017-04-06 20:56:15 +0300

    onload script fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,8 @@
         body.style.overflow = '';
 
     }
-    else {
+
+    else if (location.pathname === '/') {
 
         $(function () {
             body.classList.add('isLoadedFirst');
@@ -25,6 +26,12 @@
 
         document.cookie = "haj-visited";
 
+    }
+
+    else {
+        body.classList.add('isLoaded');
+        spinner.classList.add('hideSpinner');
+        body.style.overflow = '';
     }
 
 })();
@@ -99,3 +106,20 @@
 
 })();
 
+// Keep selected tab on page refresh
+(function () {
+    $('#myTab a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+// store the currently selected tab in the hash value
+    $("ul.nav-tabs > li > a").on("shown.bs.tab", function (e) {
+        var id = $(e.target).attr("href").substr(1);
+        window.location.hash = id;
+    });
+
+// on load of the page: switch to the currently selected tab
+    var hash = window.location.hash;
+    $('#myTab a[href="' + hash + '"]').tab('show');
+})();
